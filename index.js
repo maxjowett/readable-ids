83754b6af14d85309d40558daf9b8fbb302098f1
Max Jowett <jowett.max@gmail.com>
2020-07-01 23:00:40 -0700

    Implement bare minimal functionality

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 import { adjectives, nouns } from './lib/index.js';
+
+const getRandomAdjective = () => {
+  return adjectives[Math.floor(Math.random() * adjectives.length)];
+};
+
+const getRandomNoun = () => {
+  return nouns[Math.floor(Math.random() * nouns.length)];
+};
+
+const generateId = () => {
+  return `${getRandomAdjective()}-${getRandomNoun()}`;
+};
