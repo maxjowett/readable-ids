0cb6a76a864bdc956f785f59626acd2a6d14e7bb
Max Jowett <jowett.max@gmail.com>
2020-07-05 16:03:04 -0700

    Verify optional config if passed as argument

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 import { adjectives, nouns } from './lib/index.js';
 
+const verifyConfig = config => {
+  const { theme } = config || '';
+  const { useNumbers } = config || '';
+
+  let verifiedConfig = {
+    selectedTheme: 'dark',
+    useNumbers: false
+  };
+
+  if (theme === 'dark' || theme == 'bright') {
+    verifiedConfig.selectedTheme = theme;
+  }
+
+  if (useNumbers === true) {
+    verifiedConfig.useNumbers = true;
+  }
+
+  return generateId(verifiedConfig);
+};
+
 const getRandomAdjective = () => {
   return adjectives[Math.floor(Math.random() * adjectives.length)];
 };
@@ -8,6 +28,7 @@ const getRandomNoun = () => {
   return nouns[Math.floor(Math.random() * nouns.length)];
 };
 
-const generateId = () => {
+const generateId = config => {
+  console.log('Config: ', config);
   return `${getRandomAdjective()}-${getRandomNoun()}`;
 };
